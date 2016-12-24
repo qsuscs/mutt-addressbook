415688fc6ef2939c03ad9304de3049041570f009
Thomas Schneider <qsx@qsx.re>
2016-12-24 23:42:48 +0100

    Format source
    Signed-off-by: Thomas Schneider <qsx@qsx.re>

--- mutt-addressbook.py
+++ mutt-addressbook.py
@@ -4,17 +4,20 @@ try:
     from sys import argv
     import ldap3
 
-    LDAPDIRS=[('ldaps://ldappv.rwth-aachen.de', 'ou=People,dc=rwth-aachen,dc=de')]
+    LDAPDIRS = [
+        ('ldaps://ldappv.rwth-aachen.de', 'ou=People,dc=rwth-aachen,dc=de')
+    ]
 
-    FILTER='(mail=*)'
-    ATTRS=['cn', 'mail']
+    FILTER = '(mail=*)'
+    ATTRS = ['cn', 'mail']
 
     print('Searching … ', end='', flush=True)
     entries = []
     for d in LDAPDIRS:
         with ldap3.Connection(d[0], auto_bind=True) as conn:
             print(d[0] + ' … ', end='', flush=True)
-            flt = '(&' + FILTER + '(|(mail=' + argv[1] + '*)(cn=' + argv[1] + '*)))'
+            flt = '(&' + FILTER + \
+                '(|(mail=' + argv[1] + '*)(cn=' + argv[1] + '*)))'
             conn.search(d[1], flt, attributes=ATTRS)
             entries.extend(conn.entries)
 
