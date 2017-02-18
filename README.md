548d0638caabd773d615ddbf4003f19b6e9ba177
Thomas Schneider <qsx@qsx.re>
2017-02-18 03:37:29 +0100

    Read config from file
    Signed-off-by: Thomas Schneider <qsx@qsx.re>

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This is a script to be used as Mutt’s `query_command`.  So far it can only
 query exactly one LDAP server, but it is intended to be capable of more once
 I’m done.
+
+## Config
+Put next to the script as `config.ini` or at `~/.mutt-addressbook.ini`.  Be
+inspired by `config.sample.ini`.
--- mutt-addressbook.py
+++ mutt-addressbook.py
@@ -2,22 +2,25 @@
 
 try:
     from sys import argv
+    import configparser
+    import os
     import ldap3
 
-    LDAPDIRS = [
-        ('ldaps://ldappv.rwth-aachen.de', 'ou=People,dc=rwth-aachen,dc=de')
-    ]
+    config = configparser.ConfigParser()
+    config.read(['config.ini', os.path.expanduser('~/.mutt-addressbook.ini')])
 
     FILTER = '(mail=*)'
     ATTRS = ['cn', 'mail']
 
     print('Searching … ', end='', flush=True)
     entries = []
-    for d in LDAPDIRS:
-        with ldap3.Connection(d[0], auto_bind=True) as conn:
-            print(''.join((d[0], ' … ')), end='', flush=True)
+    for d in config:
+        if d == 'DEFAULT':
+            continue
+        with ldap3.Connection(config[d]['URI'], auto_bind=True) as conn:
+            print(''.join((d, ' … ')), end='', flush=True)
             flt = '(&{0}(|(mail={1}*)(cn={1}*)(sn={1}*)(givenName={1}*)))'.format(FILTER, argv[1])
-            conn.search(d[1], flt, attributes=ATTRS)
+            conn.search(config[d]['Base'], flt, attributes=ATTRS)
             entries.extend(conn.entries)
 
     if len(entries) == 0:
