d5093cf856d3d48d9e5d84dff2a16128187aabe6
Thomas Schneider <qsx@qsx.re>
2017-02-18 20:27:00 +0100

    Restructure for distributing
    Signed-off-by: Thomas Schneider <qsx@qsx.re>

--- README.rst
+++ README.rst
@@ -1,9 +1,12 @@
-# mutt-addressbook
+================
+mutt-addressbook
+================
 
 This is a script to be used as Mutt’s `query_command`.  So far it can only
 query exactly one LDAP server, but it is intended to be capable of more once
 I’m done.
 
-## Config
+Config
+------
 Put at `./config.ini` or at `~/.mutt-addressbook.ini`.  Be inspired by
 `config.sample.ini`.
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="mutt-addressbook",
+    version="1.0.0",
+
+    scripts=['mutt-addressbook.py'],
+
+    install_requires=['ldap3>=2.1.1'],
+
+    author="Thomas Schneider",
+    author_email="qsx+pypi@qsx.re",
+    description="An addressbook query for mutt",
+    long_description=long_description,
+    license="ISC",
+    keywords="mutt ldap addressbook address book",
+    url="https://github.com/qsuscs/mutt-addressbook",
+
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
